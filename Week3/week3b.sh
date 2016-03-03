ce5720aa26d735576053bac7091cabfdb08d74f1
Francisco García <fgarciaflorezmail@gmail.com>
2016-03-03 12:53:50 +0100

    Update week3b.sh

--- Week3/week3b.sh
+++ Week3/week3b.sh
@@ -4,4 +4,7 @@ for i in `seq 100`
 do
   ./bin/week3 -gen data/lattice-fcc-$i.json data/fcc-$i.json
   ./bin/week3 -evolve 0.1 100000 50000 data/fcc-$i.json data/fcc-evolution-$i.json
+  
+  ./bin/week3 -gen data/lattice-hexa-$i.json data/hexa-$i.json
+  ./bin/week3 -evolve 0.1 100000 50000 data/hexa-$i.json data/hexa-evolution-$i.json
 done
