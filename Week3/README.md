767320588d65875f4a5f8be8e27beae12d09e7ac
Francisco García <fgarciaflorezmail@gmail.com>
2016-03-03 12:16:54 +0100

    Week 3 readme and scipts.

--- Week3/README.md
+++ Week3/README.md
@@ -1,1 +1,7 @@
 # Week 3: Monte Carlo simulation of Hard disks in the NVT ensemble
+
+In this week assignment, two and three dimensional systems are considered; rectangular and hexagonal lattices in two dimensions, and simple cubic and face-centered cubic lattices in three dimensions.
+
+1. To generate all the data, execute ./week3.sh
+2. To generate the 2D representations, both still pictures and animations, use python/graphs.py and uncomment relevant lines.
+3. To use visual-python to visualize 3D data, use python/graphs.py as well and change the relevant file names.
--- Week3/week3.sh
+++ Week3/week3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#./bin/week3 -rect 2 10 1.0 2.0 data/rectangular.json
-#./bin/week3 -evolve 0.01 1000000 500 data/rectangular.json data/rectangular-evolution.json
+echo "Generating and evolving rectangular lattice."
+./bin/week3 -rect 2 10 1.0 2.0 data/rectangular.json
+./bin/week3 -evolve 0.01 1000000 10000 data/rectangular.json data/rectangular-evolution.json
 
 #echo "Generating plots."
 #rm report/graphs/*.png
@@ -11,20 +12,24 @@
 #rm report/graphs/rectangular-evolution.mp4
 #ffmpeg -r 60 -i report/graphs/evolution-%4d.png -vcodec libx264 -vf format=yuv420p report/graphs/rectangular-evolution.mp4
 
+echo "Generating and evolving hexagonal lattice."
 python python/lattice.py
 ./bin/week3 -gen data/lattice-hexa.json data/hexagonal.json
-./bin/week3 -evolve 0.1 1000000 1000 data/hexagonal.json data/hexagonal-evolution.json
+./bin/week3 -evolve 0.1 1000000 10000 data/hexagonal.json data/hexagonal-evolution.json
 
-#./bin/week3 -gen data/lattice-fcc.json data/fcc.json
-#./bin/week3 -evolve 0.1 300000 1000 data/fcc.json data/fcc-evolution.json
 
-#./bin/week3 -gen data/lattice-sc.json data/sc.json
-#./bin/week3 -evolve 0.1 1000000 700 data/sc.json data/sc-evolution.json
+echo "Generating and evolving FCC lattice."
+./bin/week3 -gen data/lattice-fcc.json data/fcc.json
+./bin/week3 -evolve 0.1 1000000 10000 data/fcc.json data/fcc-evolution.json
 
-echo "Generating plots."
-rm report/graphs/*.png
-python python/graphs.py
+echo "Generating and evolving SC lattice."
+./bin/week3 -gen data/lattice-sc.json data/sc.json
+./bin/week3 -evolve 0.1 1000000 10000 data/sc.json data/sc-evolution.json
 
-echo "Generating MP4."
-rm report/graphs/hexagonal-evolution.mp4
-ffmpeg -r 60 -i report/graphs/evolution-%4d.png -vcodec libx264 -vf format=yuv420p report/graphs/hexagonal-evolution.mp4
+#echo "Generating plots."
+#rm report/graphs/*.png
+#python python/graphs.py
+
+#echo "Generating MP4."
+#rm report/graphs/hexagonal-evolution.mp4
+#ffmpeg -r 60 -i report/graphs/evolution-%4d.png -vcodec libx264 -vf format=yuv420p report/graphs/hexagonal-evolution.mp4
