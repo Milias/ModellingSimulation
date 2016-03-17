801dda124bd5f1720b230724c14b94fc206705d9
Francisco García <fgarciaflorezmail@gmail.com>
2016-03-17 12:12:27 +0100

    Week 5 report

--- Week5/python/graphs.py
+++ Week5/python/graphs.py
@@ -8,8 +8,23 @@ import sys
 def PlotHistogram(filename, step):
   data = json.loads(open(filename, 'r').read())
 
+<<<<<<< HEAD
+  y = array(data["NormalizedDensity"])
+  x = array(data["BinDistances"])
+  y_avg = average(y[step:], axis=0)
+  y_avg[x>1] = y_avg[x>1.0] + 0.15
+  rho = average(array(data["BoxParticleDensity"]))
+  plt.axis([0, data["BinDistances"][-1] + 0.5*data["BinWidth"], 0, max(y_avg)])
+  plt.xlabel("r / Sphere diameter units")
+  plt.ylabel("Probability density")
+  plt.title("Normalized pair distances for\na FCC lattice with density " + r"$\rho$ = %1.2f" % rho)
+  plt.bar(x+0.5*data["BinWidth"], y_avg, data["BinWidth"], edgecolor = "none")
+  plt.savefig("report/graphs/fcc-%1.2f.pdf" % rho)
+  plt.savefig("report/graphs/fcc-%1.2f.eps" % rho)
+=======
   plt.bar(data["BinDistances"], data["NormalizedDensity"][step], data["BinWidth"])
   plt.show()
+>>>>>>> 8ebc1ce7c21bb664fa948fa5f207337baaed4f64
 
 def ParseInput(argv):
   if len(argv) > 1:
--- Week5/week5.sh
+++ Week5/week5.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-./bin/week5 -bhist ../Week4/data/evolution-fcc-fluid.json 0.0 2.5 100 data/hist-fcc.json
-python3 python/graphs.py -hist data/hist-fcc.json 100
-#./bin/week5 -dhist data/evolution-fcc-5.0.json 0.0 4.0 0.5 data/hist-fcc-5.0.json
+<<<<<<< HEAD
+#./bin/week5 -bhist ../Week4/data/evolution-fcc-1.5.json 0.0 4.0 200 data/hist-fcc-1.5.json
+#./bin/week5 -bhist ../Week4/data/evolution-fcc-2.0.json 0.0 4.0 200 data/hist-fcc-2.0.json
+#./bin/week5 -bhist ../Week4/data/evolution-fcc-2.5.json 0.0 4.0 200 data/hist-fcc-2.5.json
+#./bin/week5 -bhist ../Week4/data/evolution-fcc-3.0.json 0.0 4.0 200 data/hist-fcc-3.0.json
+#./bin/week5 -bhist ../Week4/data/evolution-fcc-4.0.json 0.0 4.0 200 data/hist-fcc-4.0.json
+#./bin/week5 -bhist ../Week4/data/evolution-fcc-5.0.json 0.0 4.0 200 data/hist-fcc-5.0.json
+python python/graphs.py -hist data/hist-fcc-1.5.json 200
+python python/graphs.py -hist data/hist-fcc-2.0.json 200
+python python/graphs.py -hist data/hist-fcc-2.5.json 200
+python python/graphs.py -hist data/hist-fcc-3.0.json 200
+python python/graphs.py -hist data/hist-fcc-4.0.json 200
+python python/graphs.py -hist data/hist-fcc-5.0.json 200
+
+#python3 python/graphs.py -hist data/hist-fcc-1.5.json 200
+=======
+./bin/week5 -dhist data/evolution-fcc-1.5.json 0.0 10.0 0.1 data/hist-fcc-1.5.json
+./bin/week5 -dhist data/evolution-fcc-5.0.json 0.0 4.0 0.5 data/hist-fcc-5.0.json
 #./bin/week5 -bhist data/evolution-fcc-1.5.json 0.0 10.0 10 data/hist-fcc-1.5.json
+>>>>>>> 8ebc1ce7c21bb664fa948fa5f207337baaed4f64
